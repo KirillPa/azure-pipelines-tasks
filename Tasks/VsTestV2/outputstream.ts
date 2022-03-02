50cf18d5469e7ef240d12831fa9d8e184305abe9
Kirill Palatnikov <kirillp@microsoft.com>
2022-03-02 12:51:32 -0800

    Removing test hooks.

--- Tasks/VsTestV2/outputstream.ts
+++ Tasks/VsTestV2/outputstream.ts
@@ -27,12 +27,10 @@ export class StringErrorWritable extends stream.Writable {
                 const taskProps: { [key: string]: string; } = { command: command};
                 ci.publishEvent(taskProps);
 
-                if (true /* TODO: this.isBlockingCommands */) {
+                if (this.isBlockingCommands) {
                     const allowedCommands = ['task.logissue', 'task.setvariable'];
                     if (allowedCommands.indexOf(command.toLowerCase()) < 0) {
-                        // TODO: Remove entire line instead of just replacing the key work.
                         line = line.replace('##vso','#vso');
-                        // line = '';
                     }
                 }
             }
